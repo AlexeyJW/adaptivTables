0915b9a646aa0bb46c968e5d167d20eaf30d1d8d
AlexeyJW <kononenko.jw@gmail.com>
2022-08-11 22:49:31 +0300

    second

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <p>There will be an adaptive table here</p>
+</template>
+
+<style>
+@import './css/reset.css';
+@import './css/font-family.css';
+@import './css/media-queries.css';
+#app {padding: 1em 0;}
+#app, body {
+  min-height: 100%;
+  width: 100%;}
+</style>
